c1f5089f81de87ea2b6794811e7e3747b78ac8a8
Brad Whitfield <bradswhitfield@gmail.com>
2018-05-20 18:58:44 -0400

    Added brief example of PID and STDOUT.

---
+++ pid1-stdout/pid1.sh
@@ -0,0 +1,8 @@
+# Start a container
+docker run -d --name httpd httpd
+
+# See that the CMD issued is PID 1, and children of it, but the exec is not
+docker exec -it httpd ps -ef
+
+# Docker logs shows the output of PID 1 and it's children, but not the exec command.
+docker logs httpd
